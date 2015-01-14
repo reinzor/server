4e22fd6d3ecb36112cfcdb5e613e77c15bf37878
Rein Appeldoorn <reinzor@gmail.com>
2015-01-14 11:59:36 +0100

    Update env.sh

--- env/env.sh
+++ env/env.sh
@@ -1,1 +1,8 @@
-echo "Server environment loaded :)"
+# Setup SERVER_ENV_DIR environment variable
+export SERVER_ENV_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Source the aliases
+source $SERVER_ENV_DIR/aliases.sh
+
+# Source the functions
+source $SERVER_ENV_DIR/functions.sh
